c20cf57253bd1a7b386f3ae75494783bcb2d2ef5
adjh54ir <adjh54ir@gmail.com>
2020-10-11 20:40:51 +0900

    first commit

---
+++ study_jpa_project/src/main/java/com/study/StudyJpaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJpaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyJpaProjectApplication.class, args);
+	}
+
+}
